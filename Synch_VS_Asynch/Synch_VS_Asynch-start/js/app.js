483ee18da87503de7465bfd87f26b52cec57d8ef
kunalarya873 <kunalarya873@gmail.com>
2024-01-29 18:56:31 +0530

    Adding Synchronous & Asynchronous

---
+++ Synch_VS_Asynch/Synch_VS_Asynch-start/js/app.js
@@ -0,0 +1,12 @@
+"use strict";
+const test = function(){
+	console.log("start of code");
+
+	alert("notice me!");
+	console.log("end of code");
+};
+const test2 = function(){
+	console.log("Now I get attention");
+};
+test();
+test2();
